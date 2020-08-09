6372ac663d62767aa8bcfa6b3c592a1e88017b62
kushberrycream <tom@wilson-express.co.uk>
2020-08-09 23:32:02 +0000

    added enviroment variables

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["MONGO_DBNAME"] = "booksReviewSite"
+os.environ["MONGO_URI"] = "mongodb+srv://kushberrycream:kushberry@myfirstcluster.cnyvx.mongodb.net/\
+booksReviewSite?retryWrites=true&w=majority"
+os.environ["SECRET_KEY"] = "secret"
