70f284241e9af297679c02fb348ad6b397abcf25
kushberrycream <tom@wilson-express.co.uk>
2020-08-09 23:54:45 +0000

    removed env.py from github and changed any sensitive info to keep security a priority

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["MONGO_DBNAME"] = "booksReviewSite"
-os.environ["MONGO_URI"] = "mongodb+srv://kushberrycream:kushberry@myfirstcluster.cnyvx.mongodb.net/\
-booksReviewSite?retryWrites=true&w=majority"
-os.environ["SECRET_KEY"] = "secret"
