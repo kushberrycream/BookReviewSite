632ec1757924d257533784756296b352a9730fb0
kushberrycream <tom@wilson-express.co.uk>
2020-08-13 00:51:50 +0000

    added edit_profile form and routed it to edit mongodb user collection

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def register():
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'user': request.form['user']})
-
-        if existing_user is None:
+        existing_email = users.find_one({'email': request.form['email']})
+        if existing_user and existing_email is None:
             hashpass = bcrypt.hashpw(
                 request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({
@@ -86,15 +86,35 @@ def profile_upload(user):
         profile_image = request.files['profile_image']
         mongo.save_file(profile_image.filename, profile_image)
         mongo.db.users.find_one_and_update({
-                'user': session['user']}, {
-                    "$set": {"profile_image": profile_image.filename}})
+            'user': session['user']}, {
+            "$set": {"profile_image": profile_image.filename}})
 
         return redirect(url_for('account', user=session['user']))
 
 
-@app.route('/file/<filename>')
-def user_file(filename):
+@app.route('/user_uploads/<filename>')
+def user_upload(filename):
     return mongo.send_file(filename)
+
+
+@app.route('/account/<user>/edit_profile')
+def edit_profile(user):
+    user = mongo.db.users.find_one({'user': session['user']})
+
+    return render_template('edit_profile.html', user=user)
+
+
+@app.route('/account/<user>/user_details', methods=['POST'])
+def user_details(user):
+    mongo.db.users.find_one_and_update({'user': session['user']}, {
+        "$set": {
+            "first_name": request.form['firstName'],
+            "last_name": request.form['lastName'],
+            "Age": request.form['age'],
+            "email": request.form['email'],
+            "favourite_book": request.form['fav-book']}})
+
+    return redirect(url_for('account', user=session['user']))
 
 
 if __name__ == "__main__":
