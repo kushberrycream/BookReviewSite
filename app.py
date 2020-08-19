8a06eeeb65d03f438c719efeedd6e5653d30222f
kushberrycream <tom@wilson-express.co.uk>
2020-08-19 00:06:38 +0000

    implimented flask-toastr, fixed all get_books endpoints

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import os
 from flask import Flask, render_template, url_for, session, redirect, request,\
-    flash
+    flash, current_app, get_flashed_messages
 from flask_pymongo import PyMongo
-
+from jinja2 import Markup, Template
 from flask_paginate import Pagination, get_page_parameter
 from bson.objectid import ObjectId
-from bson.code import Code
 import bcrypt
 if os.path.exists("env.py"):
     import env
+from flask_toastr import Toastr
 
 
 app = Flask(__name__)
+toastr = Toastr(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost")
@@ -37,10 +38,10 @@ def login():
             'password']) == login_user[
                 'password']:
             session['user'] = request.form['user']
-
+            flash('Welcome Back ' + session['user'].capitalize() + '!', 'success')
             return redirect(url_for('account', user=session['user']))
 
-    flash('Invalid username/password combination', "failed")
+    flash('Invalid username/password combination', "error")
     return redirect(url_for('homepage'))
 
 
@@ -66,7 +67,7 @@ def register():
                 'password': hashpass,
                 'email': request.form['email']})
             session['user'] = request.form['user']
-            flash('Successfully Signed Up!')
+            flash('Successfully Signed Up!', 'success')
             return redirect(url_for('account', user=session['user']))
 
     return render_template('account.html')
@@ -132,15 +133,18 @@ def user_details(user):
 
 @app.route('/all_books')
 def get_books():
-    user = mongo.db.users.find()
-    reviews = mongo.db.reviews.find()
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
+    if session.get('user') is None:
+        flash('You need to login!', 'warning')
+        return redirect(url_for('homepage'))
+    else:
+        user = mongo.db.users.find_one({'user': session['user']})
+        reviews = mongo.db.reviews.find()
+        search = False
+        q = request.args.get('q')
+        if q:
+            search = True
         per_page = 100
         page = request.args.get(get_page_parameter(), type=int, default=1)
-
         books = mongo.db.books.find().sort(
             "original_title", 1).skip((page - 1) * per_page).limit(per_page)
         pagination = get_pagination(
@@ -150,87 +154,106 @@ def get_books():
             search=search, record_name='books', format_total=True,
             format_number=True)
 
-    return render_template(
-        "books.html",
-        user=user,
-        reviews=reviews,
-        books=books,
-        pagination=pagination)
+        return render_template(
+            "books.html",
+            user=user,
+            reviews=reviews,
+            books=books,
+            pagination=pagination)
 
 
 @app.route('/all_books/year')
 def get_books_year():
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-    per_page = 100
-    page = request.args.get(get_page_parameter(), type=int, default=1)
+    if session.get('user') is None:
+        flash('You need to login!', 'warning')
+        return redirect(url_for('homepage'))
+    else:
+        user = mongo.db.users.find_one({'user': session['user']})
+        reviews = mongo.db.reviews.find()
+        search = False
+        q = request.args.get('q')
+        if q:
+            search = True
+        per_page = 100
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        books = mongo.db.books.find().sort(
+            "original_publication_year", 1).skip((
+                page - 1) * per_page).limit(per_page)
+        pagination = get_pagination(
+            per_page=per_page,
+            page=page,
+            total=books.count(),
+            search=search, record_name='books', format_total=True,
+            format_number=True)
 
-    books = mongo.db.books.find().sort(
-        "original_publication_year", -1).skip((
-            page - 1) * per_page).limit(per_page)
-    pagination = get_pagination(
-        per_page=per_page,
-        page=page,
-        total=books.count(),
-        search=search, record_name='books', format_total=True,
-        format_number=True)
-
-    return render_template(
-        "books.html",
-        books=books,
-        pagination=pagination)
+        return render_template(
+            "books.html",
+            user=user,
+            reviews=reviews,
+            books=books,
+            pagination=pagination)
 
 
 @app.route('/all_books/isbn')
 def get_books_isbn():
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-    per_page = 100
-    page = request.args.get(get_page_parameter(), type=int, default=1)
+    if session.get('user') is None:
+        flash('You need to login!', 'warning')
+        return redirect(url_for('homepage'))
+    else:
+        user = mongo.db.users.find_one({'user': session['user']})
+        reviews = mongo.db.reviews.find()
+        search = False
+        q = request.args.get('q')
+        if q:
+            search = True
+        per_page = 100
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        books = mongo.db.books.find().sort(
+            "isbn", -1).skip((page - 1) * per_page).limit(per_page)
+        pagination = get_pagination(
+            per_page=per_page,
+            page=page,
+            total=books.count(),
+            search=search, record_name='books', format_total=True,
+            format_number=True)
 
-    books = mongo.db.books.find().sort(
-        "isbn", -1).skip((
-            page - 1) * per_page).limit(per_page)
-    pagination = get_pagination(
-        per_page=per_page,
-        page=page,
-        total=books.count(),
-        search=search, record_name='books', format_total=True,
-        format_number=True)
-
-    return render_template(
-        "books.html",
-        books=books,
-        pagination=pagination)
+        return render_template(
+            "books.html",
+            user=user,
+            reviews=reviews,
+            books=books,
+            pagination=pagination)
 
 
 @app.route('/all_books/id')
 def get_books_id():
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-    per_page = 100
-    page = request.args.get(get_page_parameter(), type=int, default=1)
+    if session.get('user') is None:
+        flash('You need to login!', 'warning')
+        return redirect(url_for('homepage'))
+    else:
+        user = mongo.db.users.find_one({'user': session['user']})
+        reviews = mongo.db.reviews.find()
+        search = False
+        q = request.args.get('q')
+        if q:
+            search = True
+        per_page = 100
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        books = mongo.db.books.find().sort(
+            "id", 1).skip((page - 1) * per_page).limit(per_page)
+        pagination = get_pagination(
+            per_page=per_page,
+            page=page,
+            total=books.count(),
+            search=search, record_name='books', format_total=True,
+            format_number=True)
 
-    books = mongo.db.books.find().sort(
-        "book_id", 1).skip((
-            page - 1) * per_page).limit(per_page)
-    pagination = get_pagination(
-        per_page=per_page,
-        page=page,
-        total=books.count(),
-        search=search, record_name='books', format_total=True,
-        format_number=True)
-
-    return render_template(
-        "books.html",
-        books=books,
-        pagination=pagination)
+        return render_template(
+            "books.html",
+            user=user,
+            reviews=reviews,
+            books=books,
+            pagination=pagination)
 
 
 @app.route('/all_books/delete', methods=['POST', 'GET'])
@@ -287,6 +310,112 @@ def get_pagination(**kwargs):
         css_framework=get_css_framework(),
         link_size=get_link_size(),
         show_single_page=show_single_page_or_not(), **kwargs)
+
+
+class _toastr(object):
+    @staticmethod
+    def include_toastr_js(version=None, js_filename=None):
+        if version is None:
+            version = current_app.config.get('TOASTR_VERSION')
+        if js_filename is None:
+            js_filename = current_app.config.get('TOASTR_JS_FILENAME')
+        js = '<script src="//cdnjs.cloudflare.com/ajax/libs/' \
+             'toastr.js/%s/%s"></script>\n' % (version, js_filename)
+        return Markup(js)
+
+    @staticmethod
+    def include_toastr_css(version=None, css_filename=None):
+        if version is None:
+            version = current_app.config.get('TOASTR_VERSION')
+        if css_filename is None:
+            css_filename = current_app.config.get('TOASTR_CSS_FILENAME')
+        css = '<link href="//cdnjs.cloudflare.com/ajax/libs/' \
+              'toastr.js/%s/%s" rel="stylesheet" />\n' % (
+                  version, css_filename)
+        if current_app.config.get('TOASTR_OPACITY'):
+            return Markup(css)
+        else:
+            return Markup('''
+<style type = text/css>
+  #toast-container>div {
+    opacity: 1 !important;
+  }
+</style> %s''' % css)
+
+    @staticmethod
+    def message():
+        toastr_options = 'toastr.options.closeButton = %s; \
+        toastr.options.timeOut = %s; \
+        toastr.options.extendedTimeOut = %s; \
+        toastr.options.positionClass = \"%s\"; \
+        toastr.options.preventDuplicates = %s; \
+        toastr.options.newestOnTop = %s; \
+        toastr.options.progressBar = %s; ' % (
+            current_app.config.get('TOASTR_CLOSE_BUTTON'),
+            current_app.config.get('TOASTR_TIMEOUT'),
+            current_app.config.get('TOASTR_EXTENDED_TIMEOUT'),
+            current_app.config.get('TOASTR_POSITION_CLASS'),
+            current_app.config.get('TOASTR_PREVENT_DUPLICATES'),
+            current_app.config.get('TOASTR_NEWS_ON_TOP'),
+            current_app.config.get('TOASTR_PROGRESS_BAR'))
+        message = Template('''
+{% with messages = get_flashed_messages(with_categories=true) %}
+  {% if messages %}
+    <script type="text/javascript">
+      (function($) {
+        $(document).ready(function() {
+          {{ toastr_options }}
+          {% for category, message in messages %}
+            {% if category is undefined or category == 'message' %}
+              toastr.info(\'{{ message }}\')
+            {% else %}
+              toastr.{{ category }}(\'{{ message }}\')
+            {% endif %}
+          {% endfor %}
+        });
+      })(jQuery);
+    </script>
+  {% endif %}
+{% endwith %}
+''')
+        return Markup(render_template(message,
+                      get_flashed_messages=get_flashed_messages,
+                      toastr_options=toastr_options))
+
+
+class Toastr(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['toastr'] = _toastr
+        app.context_processor(self.context_processor)
+
+        app.config.setdefault('TOASTR_VERSION', '2.1.4')
+        app.config.setdefault('TOASTR_JQUERY_VERSION', '2.1.0')
+        app.config.setdefault('TOASTR_CSS_FILENAME', 'toastr.min.css')
+        app.config.setdefault('TOASTR_JS_FILENAME', 'toastr.min.js')
+
+        app.config.setdefault('TOASTR_CLOSE_BUTTON', 'true')
+        app.config.setdefault('TOASTR_TIMEOUT', 10000)
+        app.config.setdefault('TOASTR_EXTENDED_TIMEOUT', 1000)
+        app.config.setdefault('TOASTR_POSITION_CLASS', 'toast-top-full-width')
+        app.config.setdefault('TOASTR_PREVENT_DUPLICATES', 'false')
+        app.config.setdefault('TOASTR_NEWS_ON_TOP', 'false')
+        app.config.setdefault('TOASTR_PROGRESS_BAR', 'true')
+        app.config.setdefault('TOASTR_OPACITY', True)
+
+    @staticmethod
+    def context_processor():
+        return {
+            'toastr': current_app.extensions['toastr']
+        }
+
+    def create(self, timestamp=None):
+        return current_app.extensions['toastr'](timestamp)
 
 
 if __name__ == "__main__":
