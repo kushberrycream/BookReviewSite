99fc2cac078ad6e9adcbf0ccf46ad315853b0c38
kushberrycream <tom@wilson-express.co.uk>
2020-11-14 02:47:53 +0000

    more comments added to python code

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 app.jinja_env.add_extension("jinja2.ext.loopcontrols")
 app.jinja_env.filters["humanize"] = humanize_ts
 app.config["TOASTR_POSITION_CLASS"] = "toast-top-full-width"
+app.config["TOASTR_PREVENT_DUPLICATES"] = "true"
 
 # Pass the Flask App to the pymongo() method
 mongo = PyMongo(app)
@@ -60,6 +61,7 @@ def homepage():
 
     Returns:
         renders template homepage.html
+        
     """
     return render_template("homepage.html")
 
@@ -70,18 +72,18 @@ def register():
 
     URL used as form action attribute on the homepage register form,
     if request.method is POST then the form data is passed through various
-    validations such as existing user, existing email, valid email using 
-    isvalid() function and then the passwords not matching, if all fail then 
+    validations such as existing user, existing email, valid email using
+    isvalid() function and then the passwords not matching, if all fail then
     the password it encoded and hashed. The form data is then inserted into the
-    users database along with the hashed password and the username input is set 
+    users database along with the hashed password and the username input is set
     to session['user']
 
     Returns:
-        If existing_user found then you are redirected to the homepage with a 
+        If existing_user found then you are redirected to the homepage with a
         user already exists Toastr message.
-        If existing_email found then you are redirected to the homepage with a 
+        If existing_email found then you are redirected to the homepage with a
         email already exists Toastr message.
-        If email passed into isValid function is false then you are redirected 
+        If email passed into isValid function is false then you are redirected
         to the homepage and an invalid email message.
         If both password inputs do not match then you are again redirected to
         the homepage and a passwords do not match message is displayed.
@@ -124,25 +126,43 @@ def register():
 @app.route("/login", methods=["POST"])
 def login():
     """Login to the website.
-    
-    URL used as the form action on the login modal. 
-     """
+
+    Used as the action attribute on login form on Login modal.
+    When the login form is submitted the data is inserted into the database.
+
+    Returns:
+        If the users inputted username and password matches the password stored
+        in the database then the user is redirected to their profile.
+        If not then a Toastr error message is displayed and you stay on the
+        homepage.
+
+    """
     if request.method == "POST":
-        login_user = users.find_one({"user": request.form["user"]})
+        login_user = mongo.db.users.find_one({"user": request.form["user"]})
         if login_user:
             if (bcrypt.hashpw(request.form["pass"].encode("utf-8"),
-                          login_user["password"])
-                == login_user["password"]):
+                              login_user["password"])
+               == login_user["password"]):
                 session["user"] = request.form["user"]
-            flash("Welcome Back " + session["user"].capitalize() +
-                "!", "success")
-            return redirect(url_for("account", user=session["user"]))
-        flash("Invalid username/password combination", "error")
-        return redirect(url_for("homepage"))
+                flash("Welcome Back " + session["user"].capitalize() +
+                      "!", "success")
+                return redirect(url_for("account", user=session["user"]))
+            else:
+                flash("Invalid username/password combination", "error")
+                return redirect(url_for("homepage"))
 
 
 @app.route("/logout")
 def logout():
+    """Logout of website.
+
+    Using session.pop the variable session['user'] is removed
+    logging out the user and restricting access to some of the site.
+
+    Returns:
+        User is redirected to the homepage.
+
+    """
     session.pop("user", None)
     flash("User logged out!", "warning")
     return redirect(url_for("homepage"))
@@ -151,8 +171,24 @@ def logout():
 @app.route("/account/<user>")
 @login_required
 def account(user):
+    """ Users account page.
+
+    Once an account has been created the user is able to display a profile
+    page with any reviews they have posted or personal info. This uses the
+    @login_required decorator to make sure the user is logged in.
+
+    Args:
+        user: The current user obtained via the session['user'] variable.
+
+    Returns:
+        if arg 'user' is equal to the session['user'] then the account page is
+        displayed
+        else an error message and the homepage are displayed, this stops users
+        accessing other users accounts.
+
+    """
     if user == session['user']:
-        user = users.find_one({"user": session["user"]})
+        user = mongo.db.users.find_one({"user": session["user"]})
         reviews = review.find({"user": session["user"]}).sort("date", -1)
         return render_template("account.html", user=user, reviews=reviews)
     else:
@@ -161,58 +197,144 @@ def account(user):
 
 
 @app.route("/account/<user>/upload", methods=["POST"])
-@login_required
 def profile_image_upload(user):
+    """ Upload profile photo to database.
+
+    Used as the action attribute on the Photo upload form.
+    When the form is submitted the photo chosen is uploaded to the user
+    database within the document matching the session['user'].
+
+    Args:
+        user: The current user obtained via the session['user'] variable.
+
+    Returns:
+        redirects user back to the account page once successfully uploading
+        a profile image.
+
+    """
     if "profile_image" in request.files:
         profile_image = request.files["profile_image"]
         mongo.save_file(profile_image.filename, profile_image)
-        users.find_one_and_update(
+        mongo.db.users.find_one_and_update(
             {"user": session["user"]},
             {"$set": {"profile_image": profile_image.filename}})
 
         return redirect(url_for("account", user=session["user"]))
 
 
-@app.route("/user_uploads/<filename>")
-def user_upload(filename):
+@app.route("/user_profile_images/<filename>")
+def user_profile_images(filename):
+    """ Access users uploaded profile images
+
+    This is used as an img elements src, using url_for I open up the static
+    folder and the relevant filename stored on the database.
+    example: url_for('user_upload', filename=user['profile_image'])
+
+    Returns:
+        An instance of the response_class containing the named file, and
+        implement conditional GET semantics (using make_conditional()).
+
+    """
     return mongo.send_file(filename)
 
 
 @app.route("/delete/<id>", methods=["POST"])
-@login_required
 def delete(id):
+    """ Delete Book, User, Or Review Data.
+
+    This is used as the action attribute on all delete buttons, once selected
+    it checks if the ObjectId is in the books, user or reviews database!
+
+    Args:
+        id: dependent on which button is pressed either the User id, book id or
+          review id is passed into the form displaying the appropriate delete
+          page
+
+    Returns:
+        the delete page template is rendered with the relevant database
+        connected.
+    """
     if request.method == "POST":
-        books = book.find_one({"_id": ObjectId(id)})
-        user = users.find_one({"_id": ObjectId(id)})
-        reviews = review.find_one({"_id": ObjectId(id)})
+        books = mongo.db.books.find_one({"_id": ObjectId(id)})
+        user = mongo.db.users.find_one({"_id": ObjectId(id)})
+        reviews = mongo.db.reviews.find_one({"_id": ObjectId(id)})
         return render_template("delete.html", book=books, user=user,
                                review=reviews)
 
 
-@app.route("/account/<user>/<review_id>", methods=["POST"])
+@app.route("/account/delete/<user>/<review_id>", methods=["POST"])
 @login_required
 def delete_review(user, review_id):
-    reviews = review.find_one({"_id": ObjectId(review_id)})
-    book.find_one_and_update(
+    """ Delete a review from your account.
+
+    On the users profile the user is able to delete reviews they have
+    posted, it updates all linked databases and deletes all relevant
+    info.
+
+    Args:
+        user: The current user obtained via the session['user'] variable.
+        review_id: ObjectId passed from delete button located on account
+        page.
+
+    Returns:
+        Once the delete button is selected the user is sent back to the
+        account page
+
+    """
+    reviews = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
+    mongo.db.books.find_one_and_update(
         {"_id": ObjectId(reviews["book_id"])},
         {"$pull": {"reviews": {"review_id": ObjectId(review_id)}}})
-    users.find_one_and_update(
+    mongo.db.users.find_one_and_update(
         {"_id": ObjectId(reviews["user_id"])},
         {"$pull": {"reviews": {"review_id": ObjectId(review_id)}}})
-    review.delete_one({"_id": ObjectId(review_id)})
-    book.find_one_and_update(
+    mongo.db.reviews.delete_one({"_id": ObjectId(review_id)})
+    mongo.db.books.find_one_and_update(
         {"_id": ObjectId(reviews["book_id"])}, {"$inc": {"no_of_reviews": -1}})
     flash("Review Deleted!!!", "warning")
     return redirect(url_for("account", user=session["user"]))
 
 
-@app.route("/account/<user>/delete", methods=["POST"])
-@login_required
+@app.route("/account/delete/<user>", methods=["POST"])
 def delete_account(user):
-    users.delete_one({"user": session["user"]})
+    """ Delete the Account from the website.
+
+    The user is able to delete the whole account, it completely removed user 
+    info from the site but does not delete the reviews they have posted.
+
+    Args:
+        user: The current user obtained via the session['user'] variable.
+
+    Return:
+        Once the users account is deleted they are redirected back to the
+        homepage.
+
+    """
+    mongo.db.users.delete_one({"user": session["user"]})
     session.pop("user", None)
     flash("Account Deleted!!", "warning")
     return redirect(url_for("homepage"))
+
+
+@app.route("/all_books/delete/<book_id>", methods=["POST"])
+def delete_book(book_id):
+    """ Delete a book from the website.
+
+    If the user is the admin then they can delete all books, if the user
+    uploaded the book then they are able to delete the book.
+
+    Args:
+        book_id: ObjectId passed from the delete button located on the books
+        page.
+
+    returns:
+        Once the book has been deleted they are redirected to the all books /
+        get books page.
+
+    """
+    book.delete_one({"_id": ObjectId(book_id)})
+    flash("Book Deleted!!!", "warning")
+    return redirect(url_for("get_books"))
 
 
 @app.route("/account/<user>/edit_profile", methods=["POST"])
@@ -312,14 +434,6 @@ def get_books_search():
         record_name="books", format_total=True, format_number=True)
     return render_template(
         "books.html", books=books, pagination=pagination)
-
-
-@app.route("/all_books/delete/<book_id>", methods=["POST"])
-@login_required
-def delete_book(book_id):
-    book.delete_one({"_id": ObjectId(book_id)})
-    flash("Book Deleted!!!", "warning")
-    return redirect(url_for("get_books"))
 
 
 @app.route("/all_books/edit_book/<book_id>", methods=["POST"])
